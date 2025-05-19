5ebeb8e9599032f78f8fa8583167afc38185a43e
Savanna Bergen <savanna.bergen@gmail.com>
2025-05-19 14:40:12 -0500

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@siavanna/atomic-lib/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
