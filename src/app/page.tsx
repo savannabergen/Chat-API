b5a918deb84234fbb75e33db119fa061df241dab
Savanna Bergen <savanna.bergen@gmail.com>
2025-05-20 00:23:59 -0500

    components working well

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Button } from "@siavanna/atomic-lib"
+import { useState } from 'react';
+import { Alert, Button, Avatar } from "@siavanna/atomic-lib";
 
 export default function Home() {
+  const handleDismiss = (type: string) => {
+    console.log(`Alert of type ${type} dismissed!`);
+  };
+
   return (
     <div>
-      <Button>Hello</Button>
+      <Avatar src="https://picsum.photos/200/200" alt="Test Image" size="large" />
+      <Button>Click</Button>
     </div>
   );
 }
